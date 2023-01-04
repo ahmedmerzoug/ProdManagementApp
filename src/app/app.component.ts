8d269d9b9c3d4fae6739112cc7b67dd98fb4721d
ahmed merzoug <ahmed.merzoug@esprit.tn>
2023-01-04 16:29:10 +0100

    push

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'pm-roottag', 
+  templateUrl: './app.Component.html'
+  
+})
+
+
+
+export class AppComponent {
+  pageTitle: string = 'Page Title Value';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms'; 
+import { ConvertTospaces } from './shared/convert-tospaces_pipe';
+
+
+@NgModule({
+  declarations: [AppComponent,ProductListComponent,ConvertTospaces],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { 
+}
